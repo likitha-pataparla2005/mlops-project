e717044a0b2c60c9c031d5df10244bb1c9bea235
likitha-pataparla2005 <plikitha983@gmail.com>
2025-07-25 09:59:55 +0530

    update url

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ if st.button("🎯 Predict via API"):
             }
 
             # 🔗 API URL
-            api_url = " https://mlops-project-1-ziwm.onrender.com"  # Change this if deploying
+            api_url = " https://mlops-project-1-ziwm.onrender.com/predict"  # Change this if deploying
 
             # 🔄 POST request
             response = requests.post(api_url, json=payload)
